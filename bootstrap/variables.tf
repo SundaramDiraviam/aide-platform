b91944374f6bc469d492ba5d8637433587427a62
SundaramDiraviam <sundarsubramani94@gmail.com>
2026-05-07 17:03:23 +0530

    feat(bootstrap): add variables

---
+++ bootstrap/variables.tf
@@ -0,0 +1,2 @@
+variable "aws_region" { description = "AWS region for state resources"; type = string; default = "us-east-1" }
+variable "project"    { description = "Project name prefix"; type = string; default = "aide" }
